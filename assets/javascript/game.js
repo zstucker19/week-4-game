0f0f3bce826640f32c723a39a83f648376644687
Home <Home@homes-air.attlocal.net>
2017-04-10 13:56:55 -0700

    Reduced redundant code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,33 +6,9 @@ $(document).ready(function() {
 	var totalValue = 0;
 	var wins = 0;
 	var losses = 0;
+	var initialized = false;
 
-	for(var i = 0; i < possibleEndArray.length; i++) {
-		randomEndNum = possibleEndArray[Math.floor(Math.random() * possibleEndArray.length)];
-		$("#current-number").attr("data-openNum", randomEndNum)
-	}
 
-	for(var i = 0; i < possibleNumArray.length; i++) {
-		randomNumCrystal = possibleNumArray[Math.floor(Math.random() * possibleNumArray.length)];
-		$("#img-1").attr("data-hiddenNum", randomNumCrystal);
-	}
-
-	for(var i = 0; i < possibleNumArray.length; i++) {
-		randomNumCrystal = possibleNumArray[Math.floor(Math.random() * possibleNumArray.length)];
-		$("#img-2").attr("data-hiddenNum", randomNumCrystal);
-	}
-
-	for(var i = 0; i < possibleNumArray.length; i++) {
-		randomNumCrystal = possibleNumArray[Math.floor(Math.random() * possibleNumArray.length)];
-		$("#img-3").attr("data-hiddenNum", randomNumCrystal);
-	}
-
-	for(var i = 0; i < possibleNumArray.length; i++) {
-		randomNumCrystal = possibleNumArray[Math.floor(Math.random() * possibleNumArray.length)];
-		$("#img-4").attr("data-hiddenNum", randomNumCrystal);
-	}
-
-	$("#current-number").html(randomEndNum);
 
 	var initializeGame = function() {
 	possibleNumArray = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
@@ -70,23 +46,27 @@ $(document).ready(function() {
 
 
 	$(".crystal-image").on("click", function() {
-		var value = $(this).attr("data-hiddenNum");
-		value = parseInt(value);
-		alert(value);
-		$("#current-number").html(randomEndNum);
-		totalValue += value;
-		$("#total-number").html(totalValue);
-		if(totalValue === randomEndNum) {
-			wins++;
-			alert("Congrats!");
-			$("#win-change").html(wins);
+		if(!initialized) {
+			initialized = true;
 			initializeGame();
 		}
-		if(totalValue > randomEndNum) {
-			losses++;
-			alert("Sorry. Try Again!");
-			$("#loss-change").html(losses);
-			initializeGame();
-		}
+			var value = $(this).attr("data-hiddenNum");
+			value = parseInt(value);
+			alert(value);
+			$("#current-number").html(randomEndNum);
+			totalValue += value;
+			$("#total-number").html(totalValue);
+			if(totalValue === randomEndNum) {
+				wins++;
+				alert("Congrats!");
+				$("#win-change").html(wins);
+				initializeGame();
+			}
+			if(totalValue > randomEndNum) {
+				losses++;
+				alert("Sorry. Try Again!");
+				$("#loss-change").html(losses);
+				initializeGame();
+			}
 	});
 });
